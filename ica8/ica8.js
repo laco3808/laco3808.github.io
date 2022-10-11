ed68612501f98496730f559b357654f913d89170
Lauren Coultrip <laco3808@colorado.edu>
2022-10-11 12:20:31 -0600

    ica8

--- ica8/ica8.js
+++ ica8/ica8.js
@@ -1,6 +1,16 @@
 window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+const button = document.getElementById("CButton");
+const image = document.getElementById("image");
+button.addEventListener('click', () => {
+    console.log("button clced");
+    image.src ="../img/Croissant.jpg";
+});
+image.addEventListener('click', () => {
+    console.log("clcck");
+    window.open("https://www.youtube.com/watch?v=bh4Voxggmj8", "_blank");
+})
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
@@ -8,15 +18,4 @@ function myFunction() {
     } else {
     navbar.classList.remove("sticky");
     }
-}
-function show() {
- 
-    /* Get image and change value
-    of src attribute */
-    let image = document.getElementById("Croissant");
-
-    image.src ="../img/Croissant"
-
-    document.getElementById("CButton")
-            .style.display = "none";
 }
