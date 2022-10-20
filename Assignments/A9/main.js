ebdf29f559ac7bc62b5538a87d07b5012d552630
Lauren Coultrip <laco3808@colorado.edu>
2022-10-20 16:23:40 -0600

    A9

--- Assignments/A9/main.js
+++ Assignments/A9/main.js
@@ -5,14 +5,35 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imagesA = ['Croissant.jpg', `LiBo calligraphy.jpg`,`pic3.jpg`, `balloon-sq2.jpg`,`pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
+const Alt = {'Croissant.jpg' : 'A Croissant', 'LiBo calligraphy.jpg' : 'LiBo calligraphy','pic3.jpg' : 'Purple and white pansies','balloon-sq2.jpg' : 'Hot air balloon','pic5.jpg' : 'Large moth on a leaf'}
 
 /* Looping through images */
-
+for(const image of imagesA){
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', `../../img/${image}`);
+newImage.setAttribute('alt', Alt[image]);
 thumbBar.appendChild(newImage);
+newImage.addEventListener('click',e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const State = btn.getAttribute('class');
+    if(State==='dark'){
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+    else{
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
--- Js/ica9.js
+++ Js/ica9.js
@@ -1,20 +1,35 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', displayMessage);
+const OhNo =  document.getElementById("Text");
+        document.getElementById("Text").style.visibility="hidden";
+        const btn = document.querySelector('button');
+        btn.addEventListener('click', displayMessage);
+        function randomValueFromArray(array){
+            const random = Math.floor(Math.random()*array.length);
+            return array[random];
+        }
+        const insertX = ["GOOD JOB PRESSING THE BUTTON","WHY WOULD YOU PRESS THE BUTTON??!!","SOMEBODY DECIDED TO PRESS THE BUTTON..."];
+        
+        function displayMessage(msgText) {
+            const xItem = randomValueFromArray(insertX);
+            const body = document.body;
 
-function displayMessage() {
-    const body = document.body;
+            const panel = document.createElement('div');
+            panel.setAttribute('class','msgBox');
+            body.appendChild(panel);
 
-    const panel = document.createElement('div');
-    panel.setAttribute('class','msgBox');
-    body.appendChild(panel);
+            document.getElementById("Display").style.visibility="hidden";
+            document.getElementById("Text").innerHTML = "UH OH.YOU PRESSED IT, DIDN'T YOU...";
+            document.getElementById("Text").style.visibility="visible";
 
-    const msg = document.createElement('p');
-    msg.textContent = 'This is a message box';
-    panel.appendChild(msg);
 
-    const closeBtn = document.createElement('button');
-    closeBtn.textContent = 'x';
-    panel.appendChild(closeBtn);
-
-    closeBtn.addEventListener('click', () => panel.parentNode.removeChild(panel));
-}
+            const msg = document.createElement('p');
+            msg.textContent = xItem;
+            panel.appendChild(msg);
+            const closeBtn = document.createElement('button');
+            closeBtn.textContent = 'X';
+            panel.appendChild(closeBtn);
+            closeBtn.addEventListener('click', () => {
+                panel.parentNode.removeChild(panel);
+                document.getElementById("Display").style.visibility="visible";
+                document.getElementById("Text").style.visibility="hidden";
+            }); 
+        }
