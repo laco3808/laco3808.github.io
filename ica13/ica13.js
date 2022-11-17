51fd2eb129710e923a3bd6ade555ba47862c0584
Lauren Coultrip <laco3808@colorado.edu>
2022-11-17 13:24:25 -0700

    ica13

--- ica13/ica13.js
+++ ica13/ica13.js
@@ -1,3 +1,5 @@
+window.addEventListener('click',Gone);
+
 const aliceTumbling = [
     { transform: 'rotate(0) scale(1)' },
     { transform: 'rotate(360deg) scale(0)' }
@@ -14,9 +16,11 @@ const aliceTumbling = [
   const bread3 = document.querySelector("#bread3");
   const bread = document.querySelector("#bread");
 
+function Gone (){
 
   bread1.animate(aliceTumbling, aliceTiming).finished
     .then(() => bread.animate(aliceTumbling, aliceTiming).finished)
     .then(() => bread3.animate(aliceTumbling, aliceTiming).finished)
     .catch(error => console.error(`Error animating Alices: ${error}`));
+}
 
