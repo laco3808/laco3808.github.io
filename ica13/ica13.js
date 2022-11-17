c754f3f0d92800c1087c7899704ded70f83c3fcc
Lauren Coultrip <laco3808@colorado.edu>
2022-11-17 13:34:01 -0700

    Update ica13.js

--- ica13/ica13.js
+++ ica13/ica13.js
@@ -4,6 +4,10 @@ const aliceTumbling = [
     { transform: 'rotate(0) scale(1)' },
     { transform: 'rotate(360deg) scale(0)' }
   ];
+  const aliceTumbling2 = [
+    { transform: 'rotate(360) scale(0)' },
+    { transform: 'rotate(0) scale(1)' }
+  ];
   
   const aliceTiming = {
     duration: 2000,
@@ -21,6 +25,10 @@ function Gone (){
   bread1.animate(aliceTumbling, aliceTiming).finished
     .then(() => bread.animate(aliceTumbling, aliceTiming).finished)
     .then(() => bread3.animate(aliceTumbling, aliceTiming).finished)
+    .catch(error => console.error(`Error animating Alices: ${error}`));
+    bread1.animate(aliceTumbling2, aliceTiming).finished
+    .then(() => bread.animate(aliceTumbling2, aliceTiming).finished)
+    .then(() => bread3.animate(aliceTumbling2, aliceTiming).finished)
     .catch(error => console.error(`Error animating Alices: ${error}`));
 }
 
